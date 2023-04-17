dce9d9dca6e9a3d132e24e31aa8582e82ac8f748
Harsh Umariya <103916165+harsh-umariya@users.noreply.github.com>
2023-04-17 22:14:03 +0530

    Add files via upload

---
+++ pr1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>simple page</title>
+<head>
+<body>
+<?php
+echo"Hello World! ";
+echo"I am Harsh Umariya, I am studing computer engg. and my hubbie is playing cricaket";
+?>
+</body>
+</html>
---
+++ pr2.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<form action="welcome.php" method="post">
+First Name: <input type="text" name="fname"><br>
+Last Name: <input type="text" name="lname"><br>
+E-mail: <input type="text" name="email"><br>
+Password: <input type="password" id="pwd" name="pwd"><br>
+<input type="submit">
+</form>
+</body>
+</html>
